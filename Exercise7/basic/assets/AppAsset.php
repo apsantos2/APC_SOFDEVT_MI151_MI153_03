9273ca0e811b6e5e91425c201c65c1cf34c74a07
Albern Santos <albernsantos18@gmail.com>
2016-12-13 11:46:12 +0800

    improved folder assets

--- Exercise7/basic/assets/AppAsset.php
+++ Exercise7/basic/assets/AppAsset.php
@@ -4,11 +4,8 @@
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
-
 namespace app\assets;
-
 use yii\web\AssetBundle;
-
 /**
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
