a7cac41a0527235e373ba4dd0eb3f46e3975e308
Albern Santos <albernsantos18@gmail.com>
2016-12-13 11:53:54 +0800

    updated my web  from config folder.

--- Exercise7/basic/config/web.php
+++ Exercise7/basic/config/web.php
@@ -1,7 +1,5 @@
 <?php
-
 $params = require(__DIR__ . '/params.php');
-
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
@@ -9,7 +7,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Bern',
+            'cookieValidationKey' => 'yey',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -49,18 +47,15 @@ $config = [
     ],
     'params' => $params,
 ];
-
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
-
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
     ];
 }
-
 return $config;
