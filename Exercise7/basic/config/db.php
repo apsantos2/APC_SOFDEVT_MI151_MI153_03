02646cf07635566fbee42e1d860bcc1972be247b
Albern Santos <albernsantos18@gmail.com>
2016-12-13 11:49:25 +0800

    improved my db from config folder

--- Exercise7/basic/config/db.php
+++ Exercise7/basic/config/db.php
@@ -1,8 +1,7 @@
 <?php
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=form',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
