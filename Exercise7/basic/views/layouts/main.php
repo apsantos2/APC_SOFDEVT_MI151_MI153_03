8f63fba13583ca1125f47e2a68558914369c6f2b
Albern Santos <albernsantos18@gmail.com>
2016-12-16 12:22:41 +0800

    Fix the heading

--- Exercise7/basic/views/layouts/main.php
+++ Exercise7/basic/views/layouts/main.php
@@ -24,7 +24,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'John Rafael Furuya',
+        'brandLabel' => 'Albern Santos',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
