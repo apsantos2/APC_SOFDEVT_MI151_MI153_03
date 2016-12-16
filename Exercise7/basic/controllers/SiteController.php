5fd357d9625a526e892ceff99baedfd445cdbf5b
Albern Santos <albernsantos18@gmail.com>
2016-12-16 12:45:29 +0800

    updated codes.

--- Exercise7/basic/controllers/SiteController.php
+++ Exercise7/basic/controllers/SiteController.php
@@ -1,15 +1,15 @@
 <?php
 namespace app\controllers;
 use Yii;
-use yii\filters\AccessControl;
+use app\models\Users;
+use app\models\UsersSearch;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
-use app\models\UserFormModel;
-use app\models\Trivias;
-use yii\db\Expression;
-class SiteController extends Controller
+/**
+ * UsersController implements the CRUD actions for Users model.
+ */
+class UsersController extends Controller
 {
     /**
      * @inheritdoc
@@ -17,119 +17,95 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'logout' => ['post'],
+                    'delete' => ['POST'],
                 ],
             ],
         ];
     }
     /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-    /**
-     * Displays homepage.
-     *
-     * @return string
+     * Lists all Users models.
+     * @return mixed
      */
     public function actionIndex()
     {
-		$query = Trivias::find()
-		->orderBy(new Expression('rand()'))
-		->limit(5)
-		->all();
-		
+        $searchModel = new UsersSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         return $this->render('index', [
-			'trivias' => $query,
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
         ]);
     }
     /**
-     * Login action.
-     *
-     * @return string
+     * Displays a single Users model.
+     * @param integer $id
+     * @return mixed
      */
-    public function actionLogin()
+    public function actionView($id)
     {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
+        return $this->render('view', [
+            'model' => $this->findModel($id),
         ]);
     }
     /**
-     * Logout action.
-     *
-     * @return string
+     * Creates a new Users model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
      */
-    public function actionLogout()
+    public function actionCreate()
     {
-        Yii::$app->user->logout();
-        return $this->goHome();
-    }
-    /**
-     * Displays contact page.
-     *
-     * @return string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-            return $this->refresh();
+        $model = new Users();
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->user_id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
         }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
     }
     /**
-     * Displays about page.
-     *
-     * @return string
+     * Updates an existing Users model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
      */
-    public function actionAbout()
+    public function actionUpdate($id)
     {
-        return $this->render('about');
+        $model = $this->findModel($id);
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->user_id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
     }
-	
-	public function actionUser()	
-	{
-		$model = new UserFormModel;
-		
-		if($model->load(Yii::$app->request->post()) && $model->validate())
-		{
-			Yii::$app->session->setFlash('success','Your data has been entered successfuly');
-		}
-			return $this->render('userFormView',['model'=>$model]);
-		
-	}
+    /**
+     * Deletes an existing Users model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+        return $this->redirect(['index']);
+    }
+    /**
+     * Finds the Users model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Users the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Users::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
